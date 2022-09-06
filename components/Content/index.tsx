a3132e7611a2d44d2ff01834df5ca9cdd3c3c468
osaguild <nobuhisa.abe.0922@gmail.com>
2022-09-06 16:25:25 +0900

    add storybook

--- components/Content/index.tsx
+++ components/Content/index.tsx
@@ -4,33 +4,33 @@ import { Text, Box, Wrap, WrapItem, Center } from '@chakra-ui/react'
 const Content: FunctionComponent = () => {
   return (
     <Box>
-      <Text fontSize="6xl" textAlign="center" my="30" className="web3-title">
+      <Text fontSize="6xl" textAlign="center" my="30" className="web3-title" data-testid="text">
         welcome to web3 template
       </Text>
       <Wrap spacing="30px" justify="center" my="50">
-        <WrapItem border="2px" className="web3-background">
+        <WrapItem border="2px" className="web3-background" data-testid="item-1">
           <Center w="300px" h="300px">
             Box 1
           </Center>
         </WrapItem>
-        <WrapItem border="2px" className="web3-background">
+        <WrapItem border="2px" className="web3-background" data-testid="item-2">
           <Center w="300px" h="300px">
-            Box 1
+            Box 2
           </Center>
         </WrapItem>
-        <WrapItem border="2px" className="web3-background">
+        <WrapItem border="2px" className="web3-background" data-testid="item-3">
           <Center w="300px" h="300px">
-            Box 1
+            Box 3
           </Center>
         </WrapItem>
-        <WrapItem border="2px" className="web3-background">
+        <WrapItem border="2px" className="web3-background" data-testid="item-4">
           <Center w="300px" h="300px">
-            Box 1
+            Box 4
           </Center>
         </WrapItem>
-        <WrapItem border="2px" className="web3-background">
+        <WrapItem border="2px" className="web3-background" data-testid="item-5">
           <Center w="300px" h="300px">
-            Box 1
+            Box 5
           </Center>
         </WrapItem>
       </Wrap>
